43d9ccb6cc4302d3757f5cc97648ceb3ea0afa67
Oussama <achrafaa@email.com>
2023-10-25 13:03:16 +0100

    remove annual salary

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
+let name 
 let salary = 25000  
 function raise (salary){
 
     return salary + salary * 0.1;
     
 
-}
-function anualSalary(salary){
-
-    return salary * 12+raise(salary);
-    
 }
